465a1773ad065c148214ba8e75fb747eb5e0ac76
your name here <johnobrien8642@gmail.com>
2020-10-07 14:11:14 -0400

    Models::Visit:record_visit created and working

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,6 +1,9 @@
 
 
 class Visit < ApplicationRecord
+    validates :user_id, :shortened_url_id, presence: true
 
-
+    def self.record_visit!(user, shortened_url)
+      Visit.create!(user_id: user.id, shortened_url_id: shortened_url.id)
+    end
 end
