83ffa4f024990f6c80e483f516f4b01d0a5d2fe7
your name here <johnobrien8642@gmail.com>
2020-10-07 13:38:31 -0400

    ShortenedUrl::create_for_user_and_long_url, ::random_code created and working

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -3,4 +3,21 @@
 class ShortenedUrl < ApplicationRecord
     validates :long_url, :short_url, presence: true
     validates :short_url, uniqueness: true
+
+    
+
+    def self.create_for_user_and_long_url!(user, long_url)
+      ShortenedUrl.create!(
+          submitter_id: user.id,
+          long_url: long_url,
+          short_url: ShortenedUrl.random_code
+      )
+    end
+
+    def self.random_code
+      loop do
+        random_code = SecureRandom::urlsafe_base64
+        return random_code unless ShortenedUrl.exists?(short_url: random_code)
+      end
+    end
 end
