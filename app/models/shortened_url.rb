c14b968bd3deba1b4c69d98e987b4fd62648477b
your name here <johnobrien8642@gmail.com>
2020-10-08 18:16:21 -0400

    ::TagTopic and ::Taggings models created, linked through ::Taggings to ::ShortenedUrl

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -20,6 +20,16 @@ class ShortenedUrl < ApplicationRecord
       through: :visits,
       source: :visitor
 
+    has_many :taggings,
+      primary_key: :id, 
+      foreign_key: :shortened_url_id,
+      class_name: :Tagging,
+      dependent: :destroy
+    
+    has_many :tag_topics,
+      through: :taggings,
+      source: :tag_topic
+
 
     def self.create_for_user_and_long_url!(user, long_url)
       ShortenedUrl.create!(
--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,5 +1,17 @@
 
 
-class Tagging
+class Tagging < ApplicationRecord
+    validates :shortened_url, :tag_topic, presence: true
+    validates :shortened_url_id, uniqueness: { scope: :tag_topic_id }
+    
+    belongs_to :tag_topic,
+      primary_key: :id, 
+      foreign_key: :tag_topic_id,
+      class_name: :TagTopic
+
+    belongs_to :shortened_url,
+      foreign_key: :shortened_url_id,
+      primary_key: :id,
+      class_name: :ShortenedUrl
 
 end
